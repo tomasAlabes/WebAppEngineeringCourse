6e80036cdd208d679742f1c3fe70d4f670545528
Tomas Alabes <tomas.alabes@gmail.com>
2012-04-27 23:16:30 -0300

    rot13 exercise

--- main.py
+++ main.py
@@ -15,10 +15,56 @@
 # limitations under the License.
 #
 import webapp2
+import cgi
+
+abc = ['a','b','c','d','e','f','g','h','i','j','k',
+              'l','m','n','o','p','q','r','s','t','u','v',
+              'w','x','y','z']
+
+html = """
+<h1>Enter some text to ROT13</h1>
+<form method="post" action="/riot13">
+    <textarea name="text" cols="50" rows="7">%(rioted)s</textarea>
+    <br>
+    <input type="submit"/>
+</form>
+"""
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.out.write('Hello, Udacity!')
+        self.response.out.write("Hello udacity!")
 
-app = webapp2.WSGIApplication([('/', MainHandler)],
+class RiotHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.out.write(html % {"rioted": ''})
+    def post(self):
+         text = self.request.get('text')
+         riotedString = riot(text)
+         self.response.out.write(html % {"rioted": riotedString})
+
+def riot(string):
+    result = ""
+    for letter in string:
+        abcLength = len(abc)
+        if letter in abc:
+            ix = abc.index(letter)
+            if ix != -1:
+                newIx = ix + 13
+                if newIx >= abcLength:
+                    newIx = (newIx - abcLength)
+                result += abc[newIx]
+        else:
+            result += letter
+    escapedString = cgi.escape(result, True)
+    return escapedString
+
+def unescape(s):
+    s = s.replace("&lt;", "<")
+    s = s.replace("&gt;", ">")
+    # this has to be last:
+    s = s.replace("&amp;", "&")
+    return s
+
+app = webapp2.WSGIApplication([('/', MainHandler),
+                                ('/rot13', RiotHandler)],
                               debug=True)
